07ee5c367c4b041f96e01ae70d29a34b72952eb5
Houssam Bouazzaoui <houssam.bouazzaoui@student.hogent.be>
2022-05-13 01:20:17 +0200

    chore: comments

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ const App = () => {
   const [data,setData]= useState([]);
   const [inst,setInst] = useState();
   let [jsondata,setJsondata] = useState(new Map());
-  // if using a class, equivalent of componentDidMount
   useEffect(() => {
     WebViewer(
       {
@@ -18,30 +17,15 @@ const App = () => {
       },
       viewer.current
     ).then( async (instance) => {
-      //Bij deze code worden er features toegevoegd 
-      //en specifiek wordt er hier een filepicker toegevoegd
       const { Feature } = instance.UI;
       instance.UI.enableFeatures([Feature.FilePicker]);
-      
-      //Hier gaan we de core bibliotheek van de webviewer gebruiken
       const { documentViewer } = instance.Core;
-      //Hier wil ik de core in een globale variabele steken 
-      //om dit te kunnen gebruiken over de hele file.
       setInst(documentViewer)
-
-      //Hier gaan we luisteren naar het inladen van het document
         documentViewer.addEventListener('documentLoaded', async () => {
-          //wanneer dit gebeurd is wordt de webviewer geupdate.
           await documentViewer.getDocument().documentCompletePromise();
           documentViewer.updateView();
-          //hier gaan we de templatekeys ophalen van het document dus 
-          //alles dat in  {{}} staat. 
           const doc = documentViewer.getDocument();
           const keys = doc.getTemplateKeys();
-          //De keys worden hier gezet in een globale variabelen zodat 
-          //we ze kunnen gebruiken in andere functies.
-          //De setData zorgt ervoor dat de keys worden gebruikt voor de Textfield
-          //De setJsonData gaan we gebruiken om de values toe te passen op het word document
           keys.then(value=>{
             setData(value);
             value.forEach(v=>setJsondata(jsondata.set(v,'{{'+v+'}}')));
@@ -75,6 +59,7 @@ const applyValues= (values)=>{
           <div className='variabele'>
             <p id='varTitle'>Variabelen</p>
             {data.map(el=>
+            <div className='varTextField'>
               <TextField
                 key={el}
                 id={el}
@@ -85,13 +70,12 @@ const applyValues= (values)=>{
                 }}
                 onChange={handleChange.bind(el)}
               />
+              </div>
             )}
           </div>
         </div>
       </div>
   );
 };
-
-
 
 export default App;
